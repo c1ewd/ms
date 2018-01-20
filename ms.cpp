e725877f835f2d5555c399c5c548d04dc1129db6
clewd <c1ewd@protonmail.com>
2018-01-20 19:25:37 +0700

    public for map

--- ms.cpp
+++ ms.cpp
@@ -3,58 +3,27 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include <list>
 #include <map>
 
-#using namespace std;
-
-class BaseConfig {
-
-public:
-	virtual void Config(int id, char *key, char *buf) {};
-};
+using namespace std;
 
 class BaseDevice {
-public:
-	virtual unsigned char *Query(unsigned char *buf, int *, int *) { return 0; };
-	virtual void Request(unsigned char *, int) {};
 };
 
-class dse7320 : BaseDevice, BaseConfig {
-public:
-	void Config(int id, char *key, char *buf);
-	int x;
+class Device1 : public BaseDevice {
 };
 
-void
-dse7320::Config(int id, char *key, char *buf) {
-
-}
-
-class atysm6e : BaseDevice, BaseConfig {
-public:
-	void Config(int id, char *key, char *buf);
-	int x;
+class Device2 : public BaseDevice {
 };
-
-void
-atysm6e::Config(int id, char *key, char *buf) {
-
-}
 
 int main()
 {
-	BaseDevice* d = new BaseDevice;
-//	device d;
-	dse7320 *dse7320_obj = new dse7320;
-//	dse7320 dse7320_obj;
+	Device1* d1 = new Device1;
+	Device2* d2 = new Device2;
 
-	std::list<BaseDevice *> devices;
-	devices.push_back(d);
-	devices.push_back(d);
-	
 	map<std::string, BaseDevice*> devices;
-	devices.i
+	devices.insert(make_pair("1", d1));
+	devices.insert(make_pair("2", d2));
 
 	getchar();
     return 0;
